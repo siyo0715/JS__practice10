e712842df2d1c6a00874efa1e9eeb9538171cc3a
向山　翔 <hms0175km@gmail.com>
2022-01-09 15:57:19 +0900

    [Add]ダラーで末尾の数字にマッチ&result修正

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// コンストラクタ記法
-// const regex = new RegExp('[^0-9]', 'g')
-
-
-// リテラル記法
-// const regex = /[^0-9]/g
-
 // ^　を使って先頭の数値にマッチ
-const pattern = /^[0-9]/
+// const pattern = /^[0-9]/
+// const str = "0123456789"
+// const result = str.match(pattern)
+
+// console.log(result[0])
+
+// $で末尾の数字にマッチ
+const pattern = /[0-9]$/
 const str = "0123456789"
 const result = str.match(pattern)
 
-console.log([0])
+console.log(result[0])
