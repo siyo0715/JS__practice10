16185cc423fa7522966284dee28e2f80e72fef6c
向山　翔 <hms0175km@gmail.com>
2022-01-09 16:03:01 +0900

    [Add]*5を使って5までの数字全部をマッチ

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // const pattern = /^[0-9]/
 // const str = "0123456789"
 // const result = str.match(pattern)
-
+// _______________________________________________________________
 // console.log(result[0])
 
 // $で末尾の数字にマッチ
@@ -11,9 +11,18 @@
 // const result = str.match(pattern)
 
 // console.log(result[0])
+// _________________________________________________________________
+// *を使って全部をマッチ
+// const pattern = /[0-9]*/
+// const str = "0123456789"
+// const result = str.match(pattern)
 
-// *を使って全部マッチ
-const pattern = /[0-9]*/
+// console.log(result[0])
+
+// _________________________________________________________________
+
+// *5を使って5までの数字全部をマッチ
+const pattern = /[0-9]*5/
 const str = "0123456789"
 const result = str.match(pattern)
 
